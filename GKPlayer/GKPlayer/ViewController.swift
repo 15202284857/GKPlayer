8305fab7b82da067d75e814c4ae7c5e0d2fa9ad9
macmini-1 <0>
2018-01-29 15:58:05 +0800

    初始化项目

---
+++ GKPlayer/GKPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GKPlayer
+//
+//  Created by Mac mini-1 on 2018/1/29.
+//  Copyright © 2018年 yeqingyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GkVideoPlayer/GKPlayer.swift
@@ -0,0 +1,15 @@
+//
+//  GKPlayer.swift
+//  GKPlayer
+//
+//  Created by Mac mini-1 on 2018/1/29.
+//  Copyright © 2018年 yeqingyu. All rights reserved.
+//
+
+import UIKit
+
+class GKPlayer: NSObject {
+    static func start(){
+        print("开始播放...")
+    }
+}
